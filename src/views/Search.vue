0a7ee7cdcd1973814e60104678bb5483260ab649
xiaofei <xiaofei@botpy.com>
2020-03-08 17:45:51 +0800

    1

---
+++ src/views/Search.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <iframe
+      ref="iframe"
+      src="https://www.baidu.com"
+      width="100%"
+      :height="documentHeight"
+      frameborder="0"
+    ></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  created() {
+    this.$toast.loading({
+      duration: 0,
+      message: "加载中...",
+      forbidClick: true
+    });
+  },
+  mounted() {
+    console.dir(this.$refs.iframe);
+    this.$refs.iframe.onload = (e)=> {
+      console.log(e);
+      this.$toast.clear()
+      
+    };
+  },
+  computed: {
+    documentHeight() {
+      console.log(document.body.clientHeight);
+      return document.documentElement.clientHeight - 50 + "px";
+    }
+  }
+};
+</script>
+
+<style>
+</style>
